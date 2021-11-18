ed8b45503fafdfda48d7f4b75d2e00570113c9fc
ismail harmanda <iharmanda@hotmail.com>
2021-11-18 15:38:02 +0300

    dark mode implemented

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   const links = ["albums", "shows"];
+  const [isDark, setIsDark] = useState(false);
   const renderedNavLinks = links.map((link) => {
     return (
       <li key={link}>
@@ -12,7 +13,19 @@ const NavBar = () => {
   });
   return (
     <nav>
-      <ul>{renderedNavLinks}</ul>
+      <ul className="d-flex">
+        {renderedNavLinks}{" "}
+        <li className="my-auto">
+          <i
+            onClick={() => {
+              document.body.classList.toggle("dark-mode");
+              setIsDark((prev) => !prev);
+            }}
+            id="modeSwitcher"
+            className={`fas fa-${isDark ? "sun" : "moon"} fa-2x text-white`}
+          ></i>
+        </li>
+      </ul>
     </nav>
   );
 };
